589b2043bb3fda2aecb7e4e64104d27e88dbfec8
benjamin <healyum@users.noreply.github.com>
2020-11-24 13:28:19 +0100

    feat(world): move world creation in own file

---
+++ js/world.js
@@ -0,0 +1,63 @@
+function drawWorld() {
+	var canvasY = 0 - spriteHeight;
+	var canvasX = 0 - spriteWidth;
+
+	for (var col = 0; col < worldCols; col++) {
+	    for (var row = 0; row < worldRows; row++) {
+
+	    	if (col < SKY_LAYER) {
+	            var sourceX = Sky.posX * spriteWidth;
+	            var sourceY = Sky.posY * spriteHeight;
+	    	}
+
+	    	if (col >= SKY_LAYER && col < GRASS_LAYER) {
+	            var sourceX = Grass.posX * spriteWidth;
+	            var sourceY = Grass.posY * spriteHeight;
+	    	}
+
+	    	if (col >= GRASS_LAYER && col < DIRT_LAYER) {
+	            var sourceX = Dirt.posX * spriteWidth;
+	            var sourceY = Dirt.posY * spriteHeight;
+	    	}
+
+	    	if (col >= DIRT_LAYER && col < STONE_OR_DIRT_LAYER) {
+	    		var randomStoneOrDirt = randomNumberFromList([Stone, Dirt])
+	            var sourceX = randomStoneOrDirt.posX * spriteWidth;
+	            var sourceY = randomStoneOrDirt.posY * spriteHeight;
+	    	}
+
+	    	if (col >= STONE_OR_DIRT_LAYER && col < STONE_LAYER) {
+	            var sourceX = Stone.posX * spriteWidth;
+	            var sourceY = Stone.posY * spriteHeight;
+	            generateCoal(ctx, spriteSheetImg, row, col);
+	    	}
+
+	    	if (col >= STONE_LAYER && col < DIAMOND_LAYER) {
+	            var sourceX = Stone.posX * spriteWidth;
+	            var sourceY = Stone.posY * spriteHeight;
+	            generateDiamond(ctx, spriteSheetImg, row, col);
+	    	}
+
+	    	if (col >= DIAMOND_LAYER && col < STONE_OR_BEDROCK_LAYER) {
+	    		var randomStoneOrBedrock = randomNumberFromList([Stone, Bedrock])
+	            var sourceX = randomStoneOrBedrock.posX * spriteWidth;
+	            var sourceY = randomStoneOrBedrock.posY * spriteHeight;
+	    	}
+
+	    	if (col >= STONE_OR_BEDROCK_LAYER && col < BEDROCK_LAYER) {
+	            var sourceX = Bedrock.posX * spriteWidth;
+	            var sourceY = Bedrock.posY * spriteHeight;
+	    	}
+
+	        canvasX = row * spriteWidth;
+	        canvasY = col * spriteHeight;
+
+	        ctx.drawImage(spriteSheetImg,
+	        sourceX, sourceY, spriteWidth, spriteHeight,
+	        canvasX, canvasY, spriteWidth, spriteHeight);
+	    }
+	}
+	// TODO :  Instancier arbres sur blocs d'herbes et non position fixe
+	drawTree(ctx, spriteSheetImg, 7,4);
+	drawTree(ctx, spriteSheetImg, 12,4);
+}
