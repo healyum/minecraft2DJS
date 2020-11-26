ac657ba5b6f75f48d3fb17395a08b9e50023c3a0
benjamin <healyum@users.noreply.github.com>
2020-11-26 17:45:25 +0100

    feat(player): walk animation to the right

--- js/Player.js
+++ js/Player.js
@@ -98,7 +98,7 @@ var IdleSprite = {
 
 	render: function () {
 		var spriteList = [this.sprite1, this.sprite2, this.sprite3, this.sprite4]; // TODO : voir pour passer liste dans objet plutôt qu'instancier tableau à chaque tick
-		
+
 		ctx.clearRect(44,50, 26, 26);
 
 		ctx.drawImage(newSonicImg,
@@ -124,3 +124,73 @@ var IdleSprite = {
 }
 
 
+var WalkSprite = {
+	sprite1: {
+		x: 362,
+		y: 156,
+		width: 54,
+		height: 54,
+	},
+	sprite2: {
+		x: 429,
+		y: 156,
+		width: 54,
+		height: 54,
+	},
+	sprite3: {
+		x: 496,
+		y: 156,
+		width: 54,
+		height: 54,
+	},
+	sprite4: {
+		x: 563,
+		y: 156,
+		width: 54,
+		height: 54,
+	},
+	sprite5: {
+		x: 630,
+		y: 156,
+		width: 54,
+		height: 54,
+	},
+	sprite6: {
+		x: 697,
+		y: 156,
+		width: 54,
+		height: 54,
+	},
+
+
+	frameIndex: 0,
+	tickCount: 0,
+	ticksPerFrame: 8, // de base 60 fps, si 4 alors vitesse / 4 soit 15 fps
+	numberOfFrames: 6 || 1,
+
+	render: function () {
+		var spriteList = [this.sprite1, this.sprite2, this.sprite3, this.sprite4, this.sprite5, this.sprite6]; // TODO : voir pour passer liste dans objet plutôt qu'instancier tableau à chaque tick
+		
+		ctx.clearRect(44,50, 26, 26);
+
+		ctx.drawImage(newSonicImg,
+		spriteList[this.frameIndex].x, spriteList[this.frameIndex].y, spriteList[this.frameIndex].width, spriteList[this.frameIndex].height,
+		44, 50, 26, 26);
+	},
+
+	update: function () {
+        this.tickCount += 1;
+
+        if (this.tickCount > this.ticksPerFrame) {
+        	this.tickCount = 0;
+
+	        // If the current frame index is in range
+	        if (this.frameIndex < this.numberOfFrames - 1) {	
+	            // Go to the next frame
+	            this.frameIndex += 1;
+			} else {
+				this.frameIndex = 0;
+			}
+        }
+    }
+}
