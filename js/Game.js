fe489a67d88c7dfc976a2c6aec2273fd1d050e51
benjamin <healyum@users.noreply.github.com>
2020-11-29 23:04:08 +0100

    feat(player): check if player is away since +10s

--- js/Game.js
+++ js/Game.js
@@ -1,10 +1,26 @@
 function showFps() {
-	const now = performance.now();
+	const now = performance.now(); // return time in ms since the page loaded
 	while (times.length > 0 && times[0] <= now - 1000) {
 		times.shift();
 	}
 	times.push(now);
+
 	fps = times.length;
 	fpsId = document.getElementById('fps');
 	fpsId.innerHTML = "FPS: " + Math.round(fps); //Frames/sec
+}
+
+function isPlayerAway(isAway) {
+	var now2 = performance.now();
+	const timeAway = 10000;
+	isAway ? now2 = performance.now() : timeSinceMovement = now2;
+	/*if (isAway) {
+		now2 = performance.now();
+	} else {
+		timeSinceMovement = now2;
+	}*/
+	if (now2 > timeSinceMovement + timeAway) {
+		return true;
+	}
+	return false;
 }
